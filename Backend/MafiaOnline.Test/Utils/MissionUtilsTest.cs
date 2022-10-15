f26f411c973a886c690016d431d0070684efe78e
PatrykMargasinski <49985857+PatrykMargasinski@users.noreply.github.com>
2022-10-15 23:35:06 +0200

    Added autofixture.automoq library

--- Backend/MafiaOnline.Test/Utils/MissionUtilsTest.cs
+++ Backend/MafiaOnline.Test/Utils/MissionUtilsTest.cs
@@ -12,6 +12,9 @@ using System.Text;
 using System.Threading.Tasks;
 using Moq;
 using Pose;
+using AutoFixture;
+using AutoFixture.AutoMoq;
+using MafiaOnline.Test.Attributes;
 
 namespace MafiaOnline.Test.Repositories
 {
@@ -23,7 +26,7 @@ namespace MafiaOnline.Test.Repositories
         }
 
         [Test]
-        [AutoData]
+        [AutoMoqData]
         public void Check_If_Success_Chance_Is_Calculated_Properly(MissionUtils sut)
         {
             Agent agent = new Agent()
@@ -42,6 +45,53 @@ namespace MafiaOnline.Test.Repositories
             };
 
             Assert.AreEqual(60, sut.CalculateAgentSuccessChance(agent, mission));
+        }
+
+
+        [Test]
+        [AutoMoqData]
+        public void Check_IsMissionSuccessfullyCompleted_Method_Predicts_Success_Correctly([Frozen] Mock<IRandomizer> randomizer, MissionUtils sut)
+        {
+            randomizer.Setup(x => x.Next(It.IsAny<int>())).Returns(() => 50);
+            Agent agent = new Agent()
+            {
+                Strength = 5,
+                Dexterity = 5,
+                Intelligence = 10
+            };
+
+            Mission mission = new Mission()
+            {
+                DifficultyLevel = 10,
+                StrengthPercentage = 60,
+                DexterityPercentage = 20,
+                IntelligencePercentage = 20
+            };
+
+            Assert.IsTrue(sut.IsMissionSuccessfullyCompleted(agent, mission));
+        }
+
+        [Test]
+        [AutoMoqData]
+        public void Check_IsMissionSuccessfullyCompleted_Method_Predicts_Failure_Correctly([Frozen] Mock<IRandomizer> randomizer, MissionUtils sut)
+        {
+            randomizer.Setup(x => x.Next(It.IsAny<int>())).Returns(() => 70);
+            Agent agent = new Agent()
+            {
+                Strength = 5,
+                Dexterity = 5,
+                Intelligence = 10
+            };
+
+            Mission mission = new Mission()
+            {
+                DifficultyLevel = 10,
+                StrengthPercentage = 60,
+                DexterityPercentage = 20,
+                IntelligencePercentage = 20
+            };
+
+            Assert.IsFalse(sut.IsMissionSuccessfullyCompleted(agent, mission));
         }
     }
 }
