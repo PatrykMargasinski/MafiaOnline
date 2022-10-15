9ba74c55dc6c096f325b8d9c7f9c06336133b9ba
PatrykMargasinski <49985857+PatrykMargasinski@users.noreply.github.com>
2022-10-15 08:55:51 +0200

    Added missionUtils tests

---
+++ Backend/MafiaOnline.Test/Utils/MissionUtilsTest.cs
@@ -0,0 +1,47 @@
+﻿using AutoFixture.NUnit3;
+using MafiaOnline.BusinessLogic.Utils;
+using MafiaOnline.DataAccess.Database;
+using MafiaOnline.DataAccess.Entities;
+using MafiaOnline.DataAccess.Repositories;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using Pose;
+
+namespace MafiaOnline.Test.Repositories
+{
+    public class MissionUtilsTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        [AutoData]
+        public void Check_If_Success_Chance_Is_Calculated_Properly(MissionUtils sut)
+        {
+            Agent agent = new Agent()
+            {
+                 Strength=5,
+                 Dexterity=5,
+                 Intelligence=10
+            };
+
+            Mission mission = new Mission()
+            {
+                DifficultyLevel = 10,
+                StrengthPercentage = 60,
+                DexterityPercentage = 20,
+                IntelligencePercentage = 20
+            };
+
+            Assert.AreEqual(60, sut.CalculateAgentSuccessChance(agent, mission));
+        }
+    }
+}
